6208309dfaab4e924d735d7c86a75c18a6494d6a
piyushkummaar <pshkmr007@gmail.com>
2020-10-02 22:27:35 +0530

    'core_models'

--- core/models.py
+++ core/models.py
@@ -5,4 +5,27 @@ class Contact(models.Model):
     name = models.CharField(max_length=20)
 
     def __str__(self):
-        self.name
+        return self.name
+        
+class Services(models.Model):
+    title = models.CharField(max_length=225)
+    
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Services"
+        verbose_name_plural = "Services"
+        db_table = 'table_services'
+        
+class Subservices(models.Model):
+    services = models.ForeignKey(Services, on_delete=models.CASCADE)
+    subservices = models.CharField(max_length=225)
+    
+    def __str__(self):
+        return self.subservices
+
+    class Meta:
+        verbose_name = "Sub Services"
+        verbose_name_plural = "Sub Services"
+        db_table = 'table_subservices'
